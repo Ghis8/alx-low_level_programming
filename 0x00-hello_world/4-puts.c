bd03ec9516ae63134530814bbcc84d1e77bf1aa3
Ghis8 <ghislainkongolo@yahoo.com>
2022-04-09 09:22:26 -0700

    added betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
-/** 
- * main entry port
+#include <stdio.h>
+
+/**
+ * main - Entry point
  *
- * Return: Always zero(success)
+ * Return: Always 0 (Success  
  */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
