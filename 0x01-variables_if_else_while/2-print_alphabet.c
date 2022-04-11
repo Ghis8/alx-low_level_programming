ba759ade8f306b4cb28b6a63852f210934a79949
Ghis8 <ghislainkongolo@yahoo.com>
2022-04-11 10:09:35 -0700

    initial commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<time.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -13,7 +13,7 @@ int main(void)
 	for (low = 'a'; low <= 'z'; low++)
 		{
 			putchar(low);
-			printf("\n");
+			
 		}
 
 	return (0);
