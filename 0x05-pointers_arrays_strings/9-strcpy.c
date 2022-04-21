60da6aff3cacea860a7ac9e9af9354477eeb7050
Ghis8 <ghislainkongolo@yahoo.com>
2022-04-20 17:52:27 -0700

    incrementing index

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,14 +8,14 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char _strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, const char *src)
 {
 	int index = 0;
 
 	while (src[index])
 	{
 		dest[index] = src[index];
-		index++;
+		index += 1;
 	}
 
 	return (dest);
