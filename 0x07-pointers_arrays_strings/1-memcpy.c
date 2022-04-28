116659f35df9f4d285385d260e12db5e90d2314c
Ghis8 <ghislainkongolo@yahoo.com>
2022-04-27 20:07:46 -0700

    new variable assigned

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,3 +1,7 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies @n bytes from the memory area pointed
  *           to by @src into that pointed to by @dest.
  * @dest: A pointer to the memory area to copy @src into.
  * @src: The source buffer to copy characters from.
@@ -7,9 +11,9 @@
  */
 void *_memcpy(void *dest, void *src, size_t n)
 {
-	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
+	 int index;
+	 char *destination = dest;
+	 char *source = src;
 
 	for (index = 0; index < n; index++)
 		destination[index] = source[index];
