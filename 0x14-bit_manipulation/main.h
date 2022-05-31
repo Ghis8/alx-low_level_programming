f7b2880bfbd29b2daa0a6a905d9a893c94d98927
Ghis8 <ghislainkongolo@yahoo.com>
2022-05-30 23:59:41 -0700

    including header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,10 +1,10 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 /*
- * File: holberton.h
+ * File: main.h
  * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
+ *       written in the 0x14-bit_manipulation directory.
  */
 
 int _putchar(char c);
